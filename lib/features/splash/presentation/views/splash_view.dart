d12b65fb722dc62247717d6fc15c6bbf0efc54f3
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-02-04 17:48:18 +0200

    Add asset management and update splash screen structure

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/splash/presentation/widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
   static const String routeName = 'SplashView';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SafeArea(child: SplashViewBody()),
+    );
   }
 }
