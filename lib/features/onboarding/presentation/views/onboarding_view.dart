294f4cf381e768b1f5b6854951140ef4946a04d0
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-02-06 07:22:39 +0200

    Add OnBoardingView and OnBoardingViewBody with PageView integration

--- lib/features/onboarding/presentation/views/onboarding_view.dart
+++ lib/features/onboarding/presentation/views/onboarding_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/onboarding/presentation/views/widgets/on_boarding_view_body.dart'
+    show OnBoardingViewBody;
 
 class OnBoardingView extends StatelessWidget {
   const OnBoardingView({super.key});
   static const routeName = 'OnBoardingView';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: OnBoardingViewBody(),
+    );
   }
 }
