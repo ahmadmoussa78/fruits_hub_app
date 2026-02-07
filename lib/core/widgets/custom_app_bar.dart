7021a3dc73b12afa0d53f90103b81d3567879713
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-02-07 08:24:51 +0200

    Add LoginView and LoginViewBody with custom app bar implementation

---
+++ lib/core/widgets/custom_app_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../utils/app_text_styles.dart';
+
+AppBar buildAppBar({context, required String title}) {
+  return AppBar(
+    centerTitle: true,
+    backgroundColor: Colors.white,
+    leading: IconButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      icon: const Icon(Icons.arrow_back_ios_new),
+    ),
+    title: Text(title, style: TextStyles.bold19),
+  );
+}
