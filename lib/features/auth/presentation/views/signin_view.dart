48eba318583524834521898569c7004f71de0b8d
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-02-13 08:10:59 +0200

    Provided signincubit to signin view

--- lib/features/auth/presentation/views/signin_view.dart
+++ lib/features/auth/presentation/views/signin_view.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/services/get_it_service.dart';
 import 'package:fruits_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruits_hub/features/auth/presentation/manager/signin_cubit/signin_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/signin_view_body.dart';
+import 'package:fruits_hub/features/domain/repos/auth_repo.dart';
 
 class SigninView extends StatelessWidget {
   const SigninView({super.key});
   static const routeName = 'login';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(title: "تسجيل دخول"),
-      body: const SigninViewBody(),
+    return BlocProvider(
+      create: (context) => SigninCubit(getIt<AuthRepo>()),
+      child: Scaffold(
+        appBar: buildAppBar(title: "تسجيل دخول"),
+        body: const SigninViewBody(),
+      ),
     );
   }
 }
