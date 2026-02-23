7917d3de30421fbd47a0ba8d9f05db33c48d43fb
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-02-23 14:19:32 +0200

    Finish Featured Item

--- lib/features/home/presentation/views/widgets/custom_featured_button.dart
+++ lib/features/home/presentation/views/widgets/custom_featured_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 
 class CustomFeaturedButton extends StatelessWidget {
@@ -7,17 +8,24 @@ class CustomFeaturedButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: double.infinity,
-      height: 54,
-      child: ElevatedButton(
-        style: ButtonStyle(
-          backgroundColor: WidgetStateProperty.all(Colors.white),
-          shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      height: 32,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
           ),
+          backgroundColor: Colors.white,
         ),
         onPressed: onPressed,
-        child: const Text('تسوق الان', style: TextStyles.bold13),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: FittedBox(
+            child: Text(
+              'تسوق الان',
+              style: TextStyles.bold13.copyWith(color: AppColors.primaryColor),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/features/home/presentation/views/widgets/featured_item.dart
+++ lib/features/home/presentation/views/widgets/featured_item.dart
@@ -1,30 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart' as svg;
 import 'package:fruits_hub/core/utils/app_images.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/custom_featured_button.dart';
+import 'package:svg_flutter/svg_flutter.dart';
 
 class FeaturedItem extends StatelessWidget {
   const FeaturedItem({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.sizeOf(context).width,
-      child: Stack(
-        children: [
-          Image.asset(Assets.assetsImagesWatermelonTest),
-          Text(
-            'عروض العيد',
-            style: TextStyles.regular13.copyWith(color: Colors.white),
+    var itemWidth = MediaQuery.sizeOf(context).width;
+    return Padding(
+      padding: const EdgeInsets.only(right: 18),
+      child: SizedBox(
+        width: itemWidth,
+        child: AspectRatio(
+          aspectRatio: 342 / 158,
+          child: Stack(
+            children: [
+              Positioned(
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: itemWidth * .4,
+                child: SvgPicture.asset(
+                  Assets.assetsImagesPageViewItem2Image,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Container(
+                width: itemWidth * .5,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: svg.Svg(Assets.assetsImagesFeaturedItemBackground),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 33),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 25),
+                      Text(
+                        'عروض العيد',
+                        style: TextStyles.regular13.copyWith(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              Positioned(
+                right: 30,
+                top: 70,
+                child: Text(
+                  'خصم 25%',
+                  style: TextStyles.bold19.copyWith(
+                    color: Colors.white,
+                  ), // Adjust the text styles
+                ),
+              ),
+              const SizedBox(height: 11),
+              Positioned(
+                right: 30,
+                bottom: 20,
+                child: CustomFeaturedButton(onPressed: () {}),
+              ),
+              const SizedBox(height: 29),
+            ],
           ),
-          Text(
-            'خصم 25%',
-            style: TextStyles.bold19.copyWith(
-              color: Colors.white,
-            ), // Adjust the text styles
-          ),
-          CustomFeaturedButton(onPressed: () {}),
-        ],
+        ),
       ),
     );
   }
