b06497cfa7213fd3b8fae46c3c5c55bb40750e8b
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2026-02-24 07:11:18 +0200

    Add Fruit Item

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -4,4 +4,5 @@ abstract class AppColors {
   static const Color primaryColor = Color(0xff1F5E3B);
   static const Color lightPrimaryColor = Color(0xff2d9f5D);
   static const Color secondaryColor = Color(0xFFF4A91F);
+  static const Color lightsecondaryColor = Color(0xFFF8C76D);
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/widgets/fruit_item.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/best_selling_header.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/featuret_item_list_view.dart';
@@ -23,6 +24,8 @@ class HomeViewBody extends StatelessWidget {
               FeaturetItemListView(),
               SizedBox(height: 24),
               BestSellingHeader(),
+              SizedBox(height: 8),
+              FruitItem(),
             ],
           ),
         ),
